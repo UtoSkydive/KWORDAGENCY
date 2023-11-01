b9a6f78431375209f2eb6d4a0508e7992744bdaf
Augusto Cs <augustocsuarez1985@gmail.com>
2023-11-02 07:46:39 +1100

    pexel

--- pages/_document.js
+++ pages/_document.js
@@ -1,20 +1,40 @@
-import {Html, Head, Main, NextScript} from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 const Document = () => {
     return (
         <Html>
             <Head>
                 <link rel="icon" type="image/x-icon"
-                      href={`${process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BASEPATH ?? '' : ''}/logo.ico`}/>
-                <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
+                    href={`${process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BASEPATH ?? '' : ''}/logo.ico`} />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                 <link
                     href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap"
-                    rel="stylesheet"/>
+                    rel="stylesheet" />
+
+
+                <Script id="google-analytics">
+                    {`
+                    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-5FX3BLC4');
+                    `}
+                </Script>
+
+
             </Head>
             <body>
-            <Main/>
-            <NextScript/>
+                <Main />
+                <NextScript />
+                <noscript
+                    dangerouslySetInnerHTML={{
+                        __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5FX3BLC4"
+                        height="0" width="0" style="display:none;visibility:hidden"`,
+                    }}
+                />
             </body>
         </Html>
     )
