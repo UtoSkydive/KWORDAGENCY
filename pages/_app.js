7ed3b6b29f121fd8729696a4ec0444a00072f76c
Augusto Cs <augustocsuarez1985@gmail.com>
2023-11-03 07:37:45 +1100

    tgm

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ import '../assets/css/plugins/font-awesome.css';
 import '../assets/css/plugins/icomoon.css';
 import '../assets/css/plugins/plugins.css';
 import '../assets/scss/style.scss';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
@@ -15,8 +16,21 @@ function MyApp({ Component, pageProps }) {
       once: true,
     });
   }, []);
-  
-  return <Component {...pageProps} />;
+
+  return (
+    <>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-5FX3BLC4');
+      `}
+      </Script>
+      <Component {...pageProps} />;
+    </>
+  )
 }
 
 export default MyApp;
